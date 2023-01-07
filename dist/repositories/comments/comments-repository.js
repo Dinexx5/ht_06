8f486fe4f8927517b81722490302319413883954
Dinexx5 <Dinexx298@gmail.com>
2023-01-08 00:41:51 +0300

    get/post comments by post id

---
+++ dist/repositories/comments/comments-repository.js
@@ -0,0 +1,39 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.commentsRepository = void 0;
+const db_1 = require("../db");
+const mongodb_1 = require("mongodb");
+exports.commentsRepository = {
+    createComment(content, userId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const userDb = yield db_1.usersCollection.findOne({ _id: userId });
+            if (!userDb) {
+                return null;
+            }
+            const commentDb = {
+                _id: new mongodb_1.ObjectId(),
+                content: content,
+                createdAt: new Date().toISOString(),
+                userId: userDb._id.toString(),
+                userLogin: userDb.login
+            };
+            yield db_1.commentsCollection.insertOne(commentDb);
+            return {
+                id: commentDb._id.toString(),
+                content: commentDb.content,
+                userId: commentDb.userId,
+                userLogin: commentDb.userLogin,
+                createdAt: commentDb.createdAt
+            };
+        });
+    }
+};
