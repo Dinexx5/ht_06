ae97b38cf0656daa0348db36794e41be636a9657
Dinexx5 <Dinexx298@gmail.com>
2023-01-08 20:57:20 +0300

    jwt expiresIn fix

--- dist/application/jwt-service.js
+++ dist/application/jwt-service.js
@@ -19,7 +19,7 @@ const settings_1 = require("../settings");
 exports.jwtService = {
     createJWT(user) {
         return __awaiter(this, void 0, void 0, function* () {
-            const token = jsonwebtoken_1.default.sign({ userId: user._id }, settings_1.settings.JWT_SECRET, { expiresIn: '60' });
+            const token = jsonwebtoken_1.default.sign({ userId: user._id }, settings_1.settings.JWT_SECRET, { expiresIn: 60 });
             return token;
         });
     },
